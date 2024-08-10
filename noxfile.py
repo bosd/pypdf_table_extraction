4c7769fcc11cdf415b1aa2ea27075d0ca561b93d
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-08-10 21:20:50 +0200

    bump nox value

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
 
 package = "camelot"
 python_versions = ["3.10", "3.9", "3.8", "3.11", "3.12"]
-nox.needs_version = ">= 2021.6.6"
+nox.needs_version = ">= 2024.03.02"
 nox.options.sessions = (
     "pre-commit",
     "safety",
